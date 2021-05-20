2a8b27da0fbbee4a8a26c356424d204a6018795b
bryantchacon <bryantneta@hotmail.com>
2021-05-20 15:38:41 -0500

    Correccion de comentarios

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager sharedInstance; //Variable del singleton
+
+    public List<Sprite> prefabs = new List<Sprite>(); //Variable tipo lista que contendra sprites, la lista se declara vacia ya que los sprites se asignaran en el editor
+    public GameObject currentCandy; //Dulce seleccionado actualmente
+    public int xSize, ySize; //Tamaño del tablero en "x" y "y"
+    private GameObject[,] boardSize; //Array del tablero, es private porque solo se usara en este script, cualquiera variable que sea asi sera private
+    public bool isShifting { get; set; } //Variable que evitara que se intercambien varios dulces a la vez y que solo sea de uno en uno, como nadie mas que este mismo manager cambiara su valor, llevara get y set
+    
+    void Start()
+    {
+        if (sharedInstance == null) //Creacion del singleton del BoardManager
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject); //Esto es por si por error hay mas de 1, solo funcione el que llego primero y los demas son destruidos
+        }
+
+        Vector2 offset = currentCandy.GetComponent<BoxCollider>().size; //offset es el borde que tendra el tablero y su valor sera el alto y ancho del currentCandy, el cual se sabe por .size por medio de la obtencion de su BoxCollider, es Vector2 porque los datos son valores en "x" y "y"
+        CreateInitialBoard(offset);
+    }
+
+    private void CreateInitialBoard(Vector2 offset) //Crear tablero inicial
+    {
+
+    }
+        
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f); //static significa que la variable estara en cada uno de los caramelos, y private que solo uno de ellos usara el valor que tenga
+    private static Candy previousSelected = null; //Caramelo seleccionado previamente    
+    
+    private SpriteRenderer spriteRenderer; //Variable que hace referencia a un componente del mismo game object en el que esta este script, cuando es asi, la variable siempre sera private
+    //NOTA: Siempre que haya una variable asi, se debe inicializar en el Awake()
+    private bool isSelected = false;
+
+    public int id;
+
+    private Vector2[] adjacentDirections = new Vector2[]
+    {
+        Vector2.up,
+        Vector2.down,
+        Vector2.left,
+        Vector2.right
+    }; //Variable enum tipo array(por []) 2D(por Vector2)
+
+    private void Awake() //Se ejecuta antes del Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); //Inicializacion(obtencion del componente) de la variable
+    }
+    
+    void Start() //Se ejecuta antes del primer fps
+    {
+        
+    }
+    
+    void Update() //Se ejecuta cada fps
+    {
+        
+    }
+}
