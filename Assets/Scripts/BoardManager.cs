124cbb08c319da841a502d58492d57fcfb4b5df7
bryantchacon <bryantneta@hotmail.com>
2021-05-27 19:58:42 -0500

    Inicializacion el board y seleccion de caramelos

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,11 +6,15 @@ public class BoardManager : MonoBehaviour
 {
     public static BoardManager sharedInstance; //Variable del singleton
 
-    public List<Sprite> prefabs = new List<Sprite>(); //Variable tipo lista que contendra sprites, la lista se declara vacia ya que los sprites se asignaran en el editor
-    public GameObject currentCandy; //Dulce seleccionado actualmente
-    public int xSize, ySize; //Tamaño del tablero en "x" y "y"
-    private GameObject[,] boardSize; //Array del tablero, es private porque solo se usara en este script, cualquiera variable que sea asi sera private
+    public List<Sprite> prefabs = new List<Sprite>(); //Variable tipo lista que contendra sprites, la lista se declara vacia ya que los sprites se asignaran en el editor, la variable aparecera como un desplegable donde primero se indicara el numero de elementos que tendra y al hacerlo los campos donde iran apareceran debajo
+    public GameObject currentCandy; //Base donde ira el sprite de los caramelos(es el cuadro cafe 5_2 de los sprites)
+    public int xSize, ySize; //Variables del tamaño del tablero en "x" y "y"
+
+    private GameObject[,] candies; //Game object array del tablero, es private porque solo se usara en este script, cualquiera variable que sea asi sera private
+
     public bool isShifting { get; set; } //Variable que evitara que se intercambien varios dulces a la vez y que solo sea de uno en uno, como nadie mas que este mismo manager cambiara su valor, llevara get y set
+
+    private Candy selectedCandy;
     
     void Start()
     {
@@ -23,17 +27,41 @@ public class BoardManager : MonoBehaviour
             Destroy(gameObject); //Esto es por si por error hay mas de 1, solo funcione el que llego primero y los demas son destruidos
         }
 
-        Vector2 offset = currentCandy.GetComponent<BoxCollider>().size; //offset es el borde que tendra el tablero y su valor sera el alto y ancho del currentCandy, el cual se sabe por .size por medio de la obtencion de su BoxCollider, es Vector2 porque los datos son valores en "x" y "y"
+        Vector2 offset = currentCandy.GetComponent<BoxCollider>().size; //offset es la distancia entre cada caramelo hacia arriba y hacia abajo, y su valor sera el alto y ancho del currentCandy, el cual se sabe por .size por medio de la obtencion de su BoxCollider, es Vector2 porque los datos son valores en "x" y "y"
         CreateInitialBoard(offset);
     }
 
     private void CreateInitialBoard(Vector2 offset) //Crear tablero inicial
     {
+        candies = new GameObject[xSize, ySize]; //candies guardara un array de game object de determinadas columnas(x) y filas(y)
 
-    }
-        
-    void Update()
-    {
-        
+        float startX = this.transform.position.x;
+        float startY = this.transform.position.y;
+        //Las coordenadas a partir de las cuales se va a inicializar el board seran las del game object en el que esta este script, en este caso un game object vacio
+
+        int idx = -1; //Variable para gestionar el id de los candy, a las variables que aun no se saben que valor tendran se les asigna -1 por default
+
+        //Bucle doble para generar los candys del board
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GameObject newCandy = Instantiate(currentCandy, new Vector3(startX + (offset.x * x), startY + (offset.y * y), 0), currentCandy.transform.rotation); //Instancia un caramelo en tales coordenadas y con determinada rotacion(porque se multiplican por "x" y "y"?)
+                newCandy.name = string.Format("Candy [{0}][{1}]", x, y); //Le da nombre a cada candy, format es para darle el formato que tiene entre "", los datos entre los [] son los que van fuera de las "" y separados con , en el orden en el que se escriben y son el valor de las variables "x" y "y" en cada interacion de los bucles
+
+                do //Haz...
+                {
+                    idx = Random.Range(0, prefabs.Count); //... de manera aleatoria la eleccion del idx al elegir un numero entre 0 y el tamaño del array prefabs[]...
+                } while ((x > 0 && idx == candies[x - 1, y].GetComponent<Candy>().id) || (y > 0 && idx == candies[x, y - 1].GetComponent<Candy>().id)); //... cuando "x" o "y" sean mayor a 0 y el idx generado anteriormente sea igual al del candy que esta a la izquierda o debajo del candy actual
+                //El do while anterior empieza a funcionar a partir de la generacion del candy en la coordenada 0, 1, porque el primero en 0, 0 no tiene nada a su izquierda y debajo por ser el primer candy, en el el idx se genera de forma normal y a partir del 0, 1 se fuerza a que los candys adyascentes no coincidan por medio de sus ids, todo esto debido a que el board se genera en la 2da coordenada del plano cartesiano
+
+                Sprite sprite = prefabs[idx]; //Guarda en la variable sprite, el sprite del candy seleccionado aleatoriamente por medio del idx
+                newCandy.GetComponent<SpriteRenderer>().sprite = sprite; //Asigna el sprite elejido anteriormente al new candy generado, o sea, aqui se indica que en lugar de mostrar el sprite del curretCandy(referenciado en el editor), mostrara los del array prefabs[]
+                newCandy.GetComponent<Candy>().id = idx; //Se obtiene el script de new candy(por que todos los candy lo tienen) y se le asigna el idx como nuevo id
+
+                newCandy.transform.parent = transform; //Indica que el parent del new candy sera el game object al que esta añadido este script, o sea, el board manager, esto para que el board manager tenga un desplegable y en el se oculte el listado de todos los candys
+                candies[x, y] = newCandy; //Agrega cada candy al array, las coordenadas son el valor de las variables "x" y "y" en cada interacion de los bucles
+            }
+        }
     }
 }
--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -5,34 +5,124 @@ using UnityEngine;
 public class Candy : MonoBehaviour
 {
     private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f); //static significa que la variable estara en cada uno de los caramelos, y private que solo uno de ellos usara el valor que tenga
-    private static Candy previousSelected = null; //Caramelo seleccionado previamente    
-    
-    private SpriteRenderer spriteRenderer; //Variable que hace referencia a un componente del mismo game object en el que esta este script, cuando es asi, la variable siempre sera private
-    //NOTA: Siempre que haya una variable asi, se debe inicializar en el Awake()
+    private static Candy previousSelected = null; //Se refiere al primer candy seleccionado
+
+    private SpriteRenderer spriteRenderer; //Variable que hace referencia a un componente del mismo game object en el que esta este script, cuando es asi, la variable siempre sera private. NOTA: Siempre que haya una variable asi, se inicializa en el Awake()
     private bool isSelected = false;
 
     public int id;
 
-    private Vector2[] adjacentDirections = new Vector2[]
+    private Vector2[] adjacentDirections = new Vector2[] //Enum tipo array(por []) 2D(por Vector2)
     {
         Vector2.up,
         Vector2.down,
         Vector2.left,
         Vector2.right
-    }; //Variable enum tipo array(por []) 2D(por Vector2)
+    };
 
     private void Awake() //Se ejecuta antes del Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>(); //Inicializacion(obtencion del componente) de la variable
     }
-    
-    void Start() //Se ejecuta antes del primer fps
+
+    private void SelectCandy()
     {
-        
+        isSelected = true;
+        spriteRenderer.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Candy>(); //Guarda el script del primer candy seleccionado
+    }
+
+    private void DeselectCandy()
+    {
+        isSelected = false;
+        spriteRenderer.color = Color.white;
+        previousSelected = null;
+    }
+
+    private void OnMouseDown() //Funcion de unity. Al hacer tap/clic en el candy(este game object)...
+    {
+        if (spriteRenderer.sprite == null || BoardManager.sharedInstance.isShifting) //... si donde se hace tap/clic no hay imagen o se estan intercambiando los candys...
+        {
+            return; //... no hara nada
+        }
+        //Como no hace nada, despues de aqui el codigo no sigue
+
+        if (isSelected) //Si este candy esta seleccionado...
+        {
+            DeselectCandy(); //... se deselecciona al dar tap/clic en este mismo candy...
+        }
+        else //Si no...
+        {
+            if (previousSelected == null) //... si previousSelected esta vacio, o sea, no hay ningun candy seleccionado...
+            {
+                SelectCandy(); //... se selecciona este candy al darle tap/clic al mismo...
+            }
+            else //Si no, si previousSelected si tiene algo, o sea, si hay un candy seleccionado, pero se le da clic a otro...
+            {
+                //EL CANSWIPE EN ESTE IF MAS SU ELSE ECHAN A PERDER EL CODIGO
+                if (CanSwipe()) //... si se puede intercambiar por alguno de los que lo rodean segun la lista donde se guardan(GetAllNeighbors())...
+                {
+                    SwapSprite(previousSelected); //... intercambia el sprite de los candys recibiendo como parametro el previousSelected, o sea, el primer candy seleccionado, esta funcion se activa al darle clic/tap al segundo candy...
+                    previousSelected.DeselectCandy(); //... y el primer candy seleccionado se deselecciona
+                }
+                else //Si no, si ninguno se puede intercambiar por el, segun la lista donde seguarda(GetAllNeighbors()) o por que los caramelos estan en las esquinas que rodean al candy o lejos de el...
+                {
+                    previousSelected.DeselectCandy();
+                    SelectCandy();
+                }
+            }
+        }
+    }
+
+    public void SwapSprite(Candy firstCandySelected) //Intercambiar candys
+    {
+        if (spriteRenderer.sprite == firstCandySelected.GetComponent<SpriteRenderer>().sprite) //Si el sprite del segundo candy seleccionado(este candy) es igual al del primero seleccionado...
+        {
+            return; //... no hara nada
+        }
+        //Como no hace nada, despues de aqui el codigo no sigue
+
+        //Intercambio de sprites
+        Sprite tempSprite = firstCandySelected.spriteRenderer.sprite; //Variable auxiliar que almacena el sprite del primer candy seleccionado
+        firstCandySelected.spriteRenderer.sprite = this.spriteRenderer.sprite; //El sprite del primer candy seleccionado sera el de este, o sea, el del segundo candy seleccionado
+        this.spriteRenderer.sprite = tempSprite; //El sprite de este candy(el segundo seleccionado), sera el del primero, guardado en la variable tempSprite
+
+        //Intercambio de ids
+        int tempId = firstCandySelected.id; //Variable auxiliar que almacena el id del primer candy seleccionado
+        firstCandySelected.id = this.id; //El id del primer candy seleccionado sera el de este, o sea, el del segundo candy seleccionado
+        this.id = tempId; //El id de este candy(el segundo seleccionado), sera el del primero, guardado en la variable tempId        
+    }
+
+    //NOTA: Antes del siguiente codigo para evitar que el raycast identifique primero al propio candy, o sea, una autocolision, se va a Edit -> Project Settings -> Physics 2D y se desactiva la casilla "Queries Start in Colliders"
+    private GameObject GetNeighbor(Vector2 direction) //Funcion que obtiene el vecino, con direction(variable local y 2D por Vector2) como parametro de la direccion en la que se buscara. Debido a su tipo de dato, la funcion retornara un game object
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction); //Traza un rayo desde este candy en determinada direccion y en la variable hit guarda(por su tipo de dato) el collider del candy que detecte
+
+        if (hit.collider != null) //Si hit.collider es diferente de null, o sea, que si detecto un candy...
+        {
+            return hit.collider.gameObject; //... retorna el game object del collider que detecto, o sea, el candy que detecto
+        }
+        else //Si no detecto nada...
+        {
+            return null; //... retorna null
+        }
+    }
+
+    private List<GameObject> GetAllNeighbors() //Funcion que obtiene una lista de los candys vecinos, se llama en CanSwipe()
+    {
+        List<GameObject> neighbors = new List<GameObject>(); //Variable de la lista de los candys vecinos que guardara, se declara vacia al inicio
+
+        foreach (Vector2 direction in adjacentDirections) //Para cada direction(variable local) en cada adjacentDirections(enum tipo array 2D en este script). NOTA: En los foreach, la primer variable de los parametros es local, y se refiere al singular de la segunda variable, que sera plural, o sea, que contenga varios elementos
+        {
+            neighbors.Add(GetNeighbor(direction)); //Ejecuta la funcion GetNeighbor()(con el direction de esta funcion como parametro), y agrega a la lista el candy vecino que encuentre en cada direccion
+        }
+
+        return neighbors; //Y retorna la lista de game objects de neighbors
     }
     
-    void Update() //Se ejecuta cada fps
+    //ESTO NO FUNCIONA
+    private bool CanSwipe() //Funcion que indica con true o false si los candys se pueden intercambiar
     {
-        
+        return GetAllNeighbors().Contains(previousSelected.gameObject); //Checa si la lista que retorna GetAllNeighbors() contiene el candy actualmente seleccionado, si si lo contiene retorna true, si no, false
     }
 }
